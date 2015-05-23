2d43c506884200b209c0a11c6723c2e7c7b91a29
jessec <robertjessecooke@gmail.com>
2015-05-23 10:38:42 +0200

    init dashboard

---
+++ app/elements/core9-file/js/file.js
@@ -0,0 +1,38 @@
+File = {
+	server : {},
+	port : {},
+	db : {},
+	collection : {},
+	collectionUrl : {},
+	config : function(config) {
+		File.server = config.server;
+		File.port = config.port;
+		File.db = config.db;
+		File.collection = config.collection;
+		File.collectionUrl = config.db + config.collection;
+	},
+	check : function(){
+		return "OK";
+	},
+	collectionExists : function() {
+		System.import('../../../../bower_components/restful.js/dist/restful.min').then(function(m) {
+			//console.log('import ...');
+			//console.log(m);
+			var api = m(File.server, File.port);
+			//console.log(api);
+			api.one(File.collectionUrl, 1).get().then(function(response) {
+				//console.log(response);
+			}, function(response) {
+				// The reponse code is not >= 200 and < 400
+				//console.log(response);
+				// throw new Error('Invalid response');
+			});
+		});
+	}
+
+};
+
+export
+{
+	File
+}
