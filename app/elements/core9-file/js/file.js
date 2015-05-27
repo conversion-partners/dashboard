015b8452a8c4fb5e01d2bed03df9c8f5a3ba8fdf
jessec <robertjessecooke@gmail.com>
2015-05-27 13:33:08 +0200

    cc

--- app/elements/core9-file/js/file.js
+++ app/elements/core9-file/js/file.js
@@ -13,52 +13,66 @@ File = {
 		File.collection = config.collection;
 		File.collectionUrl = config.db + config.collection;
 	},
-	check : function(){
+	check : function() {
 		return "OK";
 	},
-	setHeaders : function(headers){
+	setHeaders : function(headers) {
 		File.headers = headers;
 	},
-	setBody : function(body){
+	setBody : function(body) {
 		File.body = body;
 	},
-	sent : function(){
-		System.import('../../../../bower_components/restful.js/dist/restful.min').then(function(m) {
-			//console.log('import ...');
-			//console.log(m);
-			var api = m(File.server)
-			//.header('AuthToken', 'test') // set global header
-			.header('Content-Type','application/x-www-form-urlencoded')
-		    .prefixUrl("testdb")
-		    .protocol('http')
-		    .port(File.port);
-			//console.log(api);
+	get : function(params) {
+		Core9.multiImport(
+				[ '../../../../bower_components/restful.js/dist/restful.min',
+						'app/elements/core9-file/js/base64' ]).then(
+				function(modules) {
+					var rest = modules[0];
+					var Base64 = Core9.unwrapModule(modules[1]);
+					console.log(params);
+					console.log(rest);
 
-			var files = api.all('mybucket.files');
 
-			files.post(File.body [{'Content-Type':'application/x-www-form-urlencoded'}, File.headers['file']]).then(function(response) {
-				console.log(response);
-			}, function(response) {
-				// The reponse code is not >= 200 and < 400
-				console.log(response);
-				// throw new Error('Invalid response');
-			});
-		});
+					var api = rest(File.server)
+				    .header("Authorization", "Basic " + Base64.encode("admin" + ":" + "changeit"))
+				    .protocol(File.port)
+				    .port(8080);
+
+					var fileCollection = api.all(File.collectionUrl);
+
+					fileCollection.getAll().then(function(response) {
+					    var fileEntities = response.body();
+
+					    fileEntities.forEach(function(commentEntity) {
+					        var file = fileEntity.data();
+					        console.log(file.body);
+					    })
+					});
+
+
+
+
+				});
 	},
 	collectionExists : function() {
-		System.import('../../../../bower_components/restful.js/dist/restful.min').then(function(m) {
-			//console.log('import ...');
-			//console.log(m);
-			var api = m(File.server, File.port);
-			//console.log(api);
-			api.one(File.collectionUrl, 1).get().then(function(response) {
-				//console.log(response);
-			}, function(response) {
-				// The reponse code is not >= 200 and < 400
-				//console.log(response);
-				// throw new Error('Invalid response');
-			});
-		});
+		System.import(
+				'../../../../bower_components/restful.js/dist/restful.min')
+				.then(
+						function(m) {
+							// console.log('import ...');
+							// console.log(m);
+							var api = m(File.server, File.port);
+							// console.log(api);
+							api.one(File.collectionUrl, 1).get().then(
+									function(response) {
+										// console.log(response);
+									}, function(response) {
+										// The reponse code is not >= 200 and <
+										// 400
+										// console.log(response);
+										// throw new Error('Invalid response');
+									});
+						});
 	}
 
 };
