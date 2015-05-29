3258ab8f08f804bf8cbab31600bce6c6b5ab01f7
trimm <robertjessecooke@gmail.com>
2015-05-29 10:16:00 +0200

    cc

--- app/elements/core9-iframe/js/child.js
+++ app/elements/core9-iframe/js/child.js
@@ -1,5 +1,12 @@
 //respond to events
 
+
+
+
+
+    $(document).ready(function(){ 
+        window.gm = jQuery("#mycanvas").gridmanager().data('gridmanager');
+        
 var domain = location.protocol +"//"+ location.host;
 window.addEventListener('message',function(event) {
 	if(event.origin !== domain) return;
@@ -7,3 +14,5 @@ window.addEventListener('message',function(event) {
 	eval(event.data);
 	event.source.postMessage('status: ...',event.origin);
 },false);
+        
+    });
