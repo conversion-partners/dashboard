46182547d04bac52b9e1c7cef233b180bdf3d7ee
trimm <robertjessecooke@gmail.com>
2015-05-29 10:10:25 +0200

    cc

--- app/elements/core9-iframe/js/child.js
+++ app/elements/core9-iframe/js/child.js
@@ -1,7 +1,9 @@
 //respond to events
+
 var domain = location.protocol +"//"+ location.host;
 window.addEventListener('message',function(event) {
 	if(event.origin !== domain) return;
 	console.log('message received:  ' + event.data,event);
-	event.source.postMessage('holla back youngin!',event.origin);
+	eval(event.data);
+	event.source.postMessage('status: ...',event.origin);
 },false);
--- app/elements/core9-iframe/js/parent.js
+++ app/elements/core9-iframe/js/parent.js
@@ -1,18 +1,16 @@
 
-var sentMessageToIframe = function (){
+var sentMessageToIframe = function (message){
 		var domain = location.protocol + "//" + location.host;
 		var iframe = document.getElementsByTagName('iframe')[0].contentWindow;
-		var message = 'Hello!  The time is: ' + (new Date().getTime());
-		console.log('blog.local:  sending message:  ' + message);
+		console.log('parent iframe:  sending message:  ' + message);
 		iframe.postMessage(message, domain); // send the message and target URI
  }
 
-setInterval(function() {
-	//sentMessageToIframe();
-}, 6000);
+
 
 
 
 window.addEventListener('iframeLoadedEvent', function (e) {
-	sentMessageToIframe();
+	var message = 'console.log("Hello!  The time is: ' + (new Date().getTime()) + '");';
+	sentMessageToIframe(message);
 }, false);
