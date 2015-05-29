6b3965d6738d6b6ba70bb3982bbdb23ddb53c601
trimm <robertjessecooke@gmail.com>
2015-05-29 17:11:33 +0200

    cc

--- test/file/customsize/customsize.js
+++ test/file/customsize/customsize.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	// HTML markup implementation, overlap mode
 	$( '#menu' ).multilevelpushmenu({
 		containersToPush: [$( '#pushobj' )],
-		menuWidth: 450, // '450px', '30em', '25%' will also work
-		menuHeight: 500
+		menuWidth: 200, // '450px', '30em', '25%' will also work
+		menuHeight: 400
 	});
 });
