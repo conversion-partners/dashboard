1c563f9b17a74b86493a16eaca6d3f2d072239e1
jessec <robertjessecooke@gmail.com>
2015-05-30 19:49:42 +0200

    cc

--- test/file/customsize/customsize.js
+++ test/file/customsize/customsize.js
@@ -5,4 +5,20 @@ $(document).ready(function(){
 		menuWidth: 200, // '450px', '30em', '25%' will also work
 		menuHeight: 400
 	});
+
+
+
+	var addItems = function(title, items){
+		var $addTo = $( '#menu' ).multilevelpushmenu( 'findmenusbytitle' , title ).first();
+		$( '#menu' ).multilevelpushmenu( 'additems' , items , $addTo , 0 );
+	}
+
+	// Remove Samsung items
+	var removeItems = function(title){
+		var item = $( '#menu' ).multilevelpushmenu( 'finditemsbyname' , title );
+		$( '#menu' ).multilevelpushmenu( 'removeitems' , item );
+	}
+
+
+
 });
