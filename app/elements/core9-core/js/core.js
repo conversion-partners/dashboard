4e37c0cf4c5a467fd500f405823adbeb0d34472f
jessec <robertjessecooke@gmail.com>
2015-05-30 23:58:39 +0200

    cc

--- app/elements/core9-core/js/core.js
+++ app/elements/core9-core/js/core.js
@@ -23,28 +23,18 @@ Core9.ajax = function(method, url, data, callback) {
 	xhr.open(method, url, true);
 	xhr.send(null);
 }
-Core9.showIframeContent = function(iframe) {
-	var document = iframe.contentDocument;
-	var serializer = new XMLSerializer();
-	var content = serializer.serializeToString(document);
-	console.log(content);
-}
-Core9.panelSwitch = function(panelSwitch, panelId) {
-	document.getElementById(panelSwitch).addEventListener('click', function() {
-		var panel = document.getElementById(panelId);
-		if (panel.style.width == '0px' || panel.style.width == '') {
-			panel.style.width = '100%'
-		} else {
-			panel.style.width = '0px';
-		}
-	}, false);
-}
 
 Core9.iframe = {
 	write : function(iframe, content) {
 		iframe.contentWindow.document.open();
 		iframe.contentWindow.document.write(content);
 		iframe.contentWindow.document.close();
+	},
+	getContent : function(iframe) {
+		var document = iframe.contentDocument;
+		var serializer = new XMLSerializer();
+		var content = serializer.serializeToString(document);
+		console.log(content);
 	}
 }
 
@@ -73,10 +63,10 @@ Core9.panel = {
 		panel.setAttribute('id', id);
 		panel.style.zIndex = zIndex.toString();
 		panel.className = "panel " + classes;
-		if(button){
+		if (button) {
 			panel.style.width = '100%';
-			panel.appendChild(Core9.panel.__activatePanelButton(panel, Core9.panel
-					.__createPanelButton()));
+			panel.appendChild(Core9.panel.__activatePanelButton(panel,
+					Core9.panel.__createPanelButton()));
 		}
 
 		return panel;
@@ -84,10 +74,12 @@ Core9.panel = {
 
 	iframe : {
 		create : function(id, zIndex, classes, content, button) {
-			var panel = Core9.panel.__createPanel(id, zIndex, classes, content, button);
-
+			var panel = Core9.panel.__createPanel(id, zIndex, classes, content,
+					button);
+			var guid = Core9.guid();
+			var content;
 			Core9.ajax('GET', content, null, function(data) {
-				var guid = Core9.guid();
+
 				var iframe = document.createElement('iframe');
 				iframe.setAttribute('id', guid);
 				iframe.className = "iframe " + classes;
@@ -97,38 +89,49 @@ Core9.panel = {
 						|| iframe.contentDocument.parentWindow;
 				iframeWindow.onload = function() {
 					window.dispatchEvent(Core9.iframeLoadedEvent);
-					var iframe = document.getElementById(guid);
-					setTimeout(function() {
+					/* not working without F12
+  					setTimeout(function() {
+						var iframe = document.getElementById(guid);
 						panel.appendChild(iframe);
 						Core9.iframe.write(iframe, data.responseText);
-					}, 1);
+					}, 1000);*/
 				};
-
+				content = data.responseText;
 			});
-
+			//Fixme too hacky!!
+			setTimeout(function() {
+				var iframe = document.getElementById(guid);
+				panel.appendChild(iframe);
+				Core9.iframe.write(iframe, content);
+			}, 1000);
 			return panel;
 		}
 	},
 	div : {
 		create : function(id, zIndex, classes, content, button) {
-			var panel = Core9.panel.__createPanel(id, zIndex, classes, content, button);
+			var panel = Core9.panel.__createPanel(id, zIndex, classes, content,
+					button);
 
-			Core9.ajax('GET', content, null, function(data) {
-				var guid = Core9.guid();
-				 var div = document.createElement('div');
-				 div.innerHTML = data.responseText.replace('evalscript', 'evalscript-'+guid);
-				 panel.appendChild(div);
-				 var x = document.getElementById('evalscript-'+guid).innerHTML;
-				 eval(x);
-			});
-
+			Core9
+					.ajax('GET', content, null,
+							function(data) {
+								var guid = Core9.guid();
+								var div = document.createElement('div');
+								div.innerHTML = data.responseText.replace(
+										'evalscript', 'evalscript-' + guid);
+								panel.appendChild(div);
+								var x = document.getElementById('evalscript-'
+										+ guid).innerHTML;
+								eval(x);
+							});
 
 			return panel;
 		}
 	},
 
 	create : function(id, zIndex, classes, type, content, button) {
-		return Core9.panel[type]['create'](id, zIndex, classes, content, button);
+		return Core9.panel[type]['create']
+				(id, zIndex, classes, content, button);
 	},
 	remove : function(id) {
 	},
