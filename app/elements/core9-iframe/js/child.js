1cf300e3b05f2586ecf14b9b1cc721e5572c1d7a
jessec <robertjessecooke@gmail.com>
2015-05-30 19:54:53 +0200

    cc

--- app/elements/core9-iframe/js/child.js
+++ app/elements/core9-iframe/js/child.js
@@ -1,18 +1,10 @@
 //respond to events
 
-
-
-
-
-    $(document).ready(function(){
-
-
-var domain = location.protocol +"//"+ location.host;
-window.addEventListener('message',function(event) {
-	if(event.origin !== domain) return;
-	console.log('message received:  ' + event.data,event);
+var domain = location.protocol + "//" + location.host;
+window.addEventListener('message', function(event) {
+	if (event.origin !== domain)
+		return;
+	console.log('message received:  ' + event.data, event);
 	eval(event.data);
-	event.source.postMessage('status: ...',event.origin);
-},false);
-
-    });
+	event.source.postMessage('status: ...', event.origin);
+}, false);
