31cb065481b06d1717bf6aa2feba9c875328ad9c
jessec <robertjessecooke@gmail.com>
2015-05-31 07:39:04 +0200

    cc

--- app/elements/core9-iframe/js/child.js
+++ app/elements/core9-iframe/js/child.js
@@ -1,18 +1,19 @@
-//respond to events
-
-var domain = location.protocol + "//" + location.host;
-window.addEventListener('message', function(event) {
-	if (event.origin !== domain)
-		return;
-	console.log('message received:  ' + event.data, event);
-	eval(event.data);
-	event.source.postMessage('status: ...', event.origin);
-}, false);
-
-sentMessageToParent = function(message){
-	parent.postMessage(message,domain);
+var listenToPostMessages = function() {
+	var eventMethod = window.addEventListener ? "addEventListener"
+			: "attachEvent";
+	var eventer = window[eventMethod];
+	var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+	eventer(messageEvent, function(e) {
+		var domain = location.protocol + "//" + location.host;
+		if (e.origin !== domain)
+			return;
+		console.log('child received message!:  ', e.data);
+		sentMessageToParent('child received message!:  ' +  e.data);
+	}, false);
 }
+listenToPostMessages();
 
-/*setInterval(function() {
-	parent.postMessage("Hello",domain);
-},1000);*/
+sentMessageToParent = function(message) {
+	var domain = location.protocol + "//" + location.host;
+	parent.postMessage(message, domain);
+}
--- app/elements/core9-iframe/js/parent.js
+++ app/elements/core9-iframe/js/parent.js
@@ -1,17 +1,20 @@
 
-var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-var eventer = window[eventMethod];
-var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+var listenToPostMessages = function(){
+	var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+	var eventer = window[eventMethod];
+	var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
 
-// Listen to message from child window
-eventer(messageEvent,function(e) {
-  console.log('parent received message!:  ',e.data);
-},false);
+	// Listen to message from child window
+	eventer(messageEvent,function(e) {
+	  console.log('parent received message!:  ',e.data);
+	},false);
+}
+listenToPostMessages();
 
-
-var sentMessageToIframe = function (iframe, message){
+var sentMessageToIframe = function (message, iframe){
 		var domain = location.protocol + "//" + location.host;
-
+		console.log('parent sending message : ');
+		console.log(message);
 		if(arguments.length == 1){
 			var iframes = document.getElementsByTagName('iframe');
 			for (var int = 0; int < iframes.length; int++) {
