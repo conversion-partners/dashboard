7445b2e5b9bf14a457c68c4d88d1417cef7ee240
jessec <robertjessecooke@gmail.com>
2015-05-31 20:26:38 +0200

    cc

--- test/snippets/eval-script.js
+++
@@ -1,9 +0,0 @@
-
-		/* 		ajax('GET', 'menu.html', null, function(data) {
-		 var menu = document.createElement('div');
-		 menu.innerHTML = data.responseText;
-		 document.body.appendChild(menu);
-		 var x = document.getElementById("evalscript").innerHTML;
-		 eval(x);
-		 DragDrop.bind(document.getElementById('contextmenu'));
-		 }); */
