cc725cf1ca15fb0e7b1a4a9d8034fc11ca55380d
jessec <robertjessecooke@gmail.com>
2015-06-22 22:18:57 +0200

    cc

--- app/elements/core9-core/js/core-db.js
+++ app/elements/core9-core/js/core-db.js
@@ -21,15 +21,20 @@ Core9.db = {
 		}
 		return this;
 	},
-	creatCollection : function(collection, callback){
-		//http PUT 127.0.0.1:8080/myfirstdb/myfirstcoll desc='this is my first collection created with restheart'
-		
+	createCollection : function(collection, callback){
 		Core9.db.__do('PUT',Core9.db.__config.dburl+collection).then(function(response) {
 			  callback(response);
 			}, function(error) {
 				callback(error);
 			});	
-		
+	},
+	deleteCollection : function(collection, db, callback){
+		var url = Core9.db.__config.protocol+Core9.db.__config.host+':'+Core9.db.__config.port+'/'+db+'/'+collection;
+		Core9.db.__do('DELETE',url).then(function(response) {
+			  callback(response);
+			}, function(error) {
+				callback(error);
+			});	
 	},
 	getCollections : function(callback) {
 		Core9.db.__do('GET',Core9.db.__config.dburl).then(function(response) {
@@ -39,11 +44,12 @@ Core9.db = {
 		  console.error(error);
 		});
 	},
-	__do : function(method, url) {
+	__do : function(method, url, etag) {
 		return new Promise(function(resolve, reject) {
 			var req = new XMLHttpRequest();
 			req.open(method, url);
 			req.setRequestHeader("Content-Type", "application/hal+json; charset=utf-8");
+			req.setRequestHeader("If-Match", etag); // to delete/put you need the etag of the to be deleted/updated resource!
 			req.setRequestHeader("Authorization", "Basic " + btoa(Core9.db.__config.username + ":" + Core9.db.__config.password));
 			req.onload = function() {
 				if (req.status == 200) {
