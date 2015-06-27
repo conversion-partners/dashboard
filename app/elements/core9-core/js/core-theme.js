45c4b46012dad669cb95a9c11b3a3867addc8d6d
jessec <rcooke@core9.io>
2015-06-27 09:49:18 +0200

    cc

--- app/elements/core9-core/js/core-theme.js
+++ app/elements/core9-core/js/core-theme.js
@@ -3,6 +3,31 @@ if (typeof Core9 === 'undefined') {
 };
 
 Core9.theme = {
+    get : function(){
+
+    },
+
+    __do : function(method, url) {
+      return new Promise(function(resolve, reject) {
+        var req = new XMLHttpRequest();
+        req.open(method, url);
+        req.setRequestHeader("Content-Type",
+            "application/hal+json; charset=utf-8");
+        req.onload = function() {
+          if (req.status == 200) {
+            resolve(req.response);
+          } else {
+            reject(Error(req.statusText));
+          }
+        };
+        req.onerror = function() {
+          reject(Error("Network Error"));
+        };
+        req.send();
+
+      });
+    }
+
 }
 
 
