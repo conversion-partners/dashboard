5254751e37eb24f23209f3d0d9e5e5ddcc50baf8
jessec <rcooke@core9.io>
2015-06-27 11:51:24 +0200

    cc

--- app/elements/core9-core/js/core-theme.js
+++ app/elements/core9-core/js/core-theme.js
@@ -3,16 +3,47 @@ if (typeof Core9 === 'undefined') {
 };
 
 Core9.theme = {
-    get : function(){
+    __repo : {
+      themes : []
+    },
+    get : function(callback){
+        this.__async([this.__do('GET', '/dashboard/themes/bower.json'),this.__do('GET', '/dashboard/themes/bower_components/core9-theme-example/bower.json')],callback);
+    },
+    __parseBower : function (callback) {
+
+    this.__do('GET', '/dashboard/themes/bower.json').then(function(response) {
+      Core9.theme.__repo.bower = JSON.parse(response);
+        var obj = Core9.theme.__repo.bower.dependencies;
+        for (var prop in obj) {
+          if( obj.hasOwnProperty( prop ) ) {
+            var theme = {
+              "name" : prop,
+              "version" : obj[prop]
+            }
+            Core9.theme.__repo.themes.push(theme);
+          }
+        }
+        callback(Core9.theme.__repo);
+      }, function(error) {
+        console.log(error);
+      });
 
     },
-
-    __do : function(method, url) {
+    __async : function(iterable, callback){
+      Promise.all(iterable)
+       .then(values => {
+           callback(values);
+        });
+    },
+    __do : function(method, url,contentType) {
       return new Promise(function(resolve, reject) {
         var req = new XMLHttpRequest();
         req.open(method, url);
+        if(contentType == null){
+          contentType = "application/json;";
+        }
         req.setRequestHeader("Content-Type",
-            "application/hal+json; charset=utf-8");
+            contentType + " charset=utf-8");
         req.onload = function() {
           if (req.status == 200) {
             resolve(req.response);
