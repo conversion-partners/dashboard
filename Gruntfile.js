f021c197aec1fc0eafa0809a6871c993277f7dbf
jessec <rcooke@core9.io>
2015-06-29 20:14:50 +0200

    cc

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    express: {
+      options: {
+        // Override defaults here
+      },
+      web: {
+        options: {
+          script: 'backend/app.js',
+        }
+      },
+    },
+    watch: {
+      frontend: {
+        options: {
+          livereload: true
+        },
+        files: [
+          // triggering livereload when the .css file is updated
+          // (compared to triggering when sass completes)
+          // allows livereload to not do a full page refresh
+          'frontend/static/styles/*.css',
+          'frontend/templates/**/*.jade',
+          'frontend/static/scripts/**/*.js',
+          'frontend/static/img/**/*'
+        ]
+      },
+      stylesSass: {
+        files: [
+          '!frontend/styles/sass-twitter-bootstrap/',
+          'frontend/styles/**/*.scss'
+        ],
+        tasks: [
+          'compass'
+        ]
+      },
+      web: {
+        files: [
+          'backend/**/*.js',
+          'config/*',
+          'test/**/*.js',
+        ],
+        tasks: [
+          'express:web'
+        ],
+        options: {
+          nospawn: true, //Without this option specified express won't be reloaded
+          atBegin: true,
+        }
+      }
+    },
+    parallel: {
+      web: {
+        options: {
+          stream: true
+        },
+        tasks: [{
+          grunt: true,
+          args: ['watch:frontend']
+        }, {
+          grunt: true,
+          args: ['watch:stylesSass']
+        }, {
+          grunt: true,
+          args: ['watch:web']
+        }]
+      },
+    }
+  });
+  grunt.registerTask('web', 'launch webserver and watch tasks', [
+    'parallel:web',
+  ]);
+
+  grunt.registerTask('default', ['web']);
+};
