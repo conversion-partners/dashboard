e13dfc544194e1d21b706edc4f36b7113ee315ae
jessec <rcooke@core9.io>
2015-07-05 18:58:42 +0200

    cc

--- app/elements/core9-core/js/core-blocks.js
+++ app/elements/core9-core/js/core-blocks.js
@@ -1,11 +1,24 @@
-
 if (typeof Core9 === 'undefined') {
-	Core9 = {}
+  Core9 = {}
 };
 
-Core9.blocks = function(){
-    console.log(data.template);
-}
+Core9.blocks = function() {
+  var data = {
+    template: "/dashboard/data/accounts/easydrain/themes/bower_components/core9-theme-ess/templates/pages/home/versions/blue/index.html"
+  };
+  console.log(data.template);
 
+  var rows = document.getElementsByClassName('row');
+
+  for (var i = 0; i < rows.length; i++) {
+      var columns = rows[i].getElementsByClassName("column");
+      for (var i = 0; i < columns.length; i++) {
+        var column = columns[i];
+        console.log(column);
+      }
+  }
+
+
+}
 
 Core9.blocks();
