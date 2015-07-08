54794e781076a91ded12b8c2131dbe8663943dfc
jessec <rcooke@core9.io>
2015-07-08 22:40:22 +0200

    cc

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ var express = require('express');
 var app = express();
 
 
+//app.use('/dashboard/data/accounts/.*', express.static('.'));
+
+
+app.use('/dashboard/api/file*', function (req, res) {
+  console.log(req);
+  res.send(req.originalUrl);
+});
+
 
 app.use('/dashboard', express.static('.'));
 
 ///dashboard/data/accounts/easydrain/themes/bower_components/core9-theme-ess/templates/pages/Home/versions/blue/index.html
 
-app.use('/dashboard/data/accounts/.*', express.static('.'));
+
 
 
 app.use('*', function (req, res) {
