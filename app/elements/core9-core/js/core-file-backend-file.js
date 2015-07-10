05409c2fd22df5233f1d4323303dbabd154cef89
Robert Cooke <robert.cooke@trimm.nl>
2015-07-10 12:30:00 +0200

    cc

---
+++ app/elements/core9-core/js/core-file-backend-file.js
@@ -0,0 +1,44 @@
+if (typeof Core9 === 'undefined') {
+	Core9 = {}
+};
+if (typeof Core9.file === 'undefined') {
+	Core9.file = {}
+};
+if (typeof Core9.file.backend === 'undefined') {
+	Core9.file.backend = {}
+};
+
+Core9.file.backend.file = {
+
+  create = function(){
+
+  },
+  read = function(){
+
+  },
+  update = function(){
+
+  },
+  delete = function(){
+
+  },
+  rename = function(){
+
+  },
+  copy = function(){
+
+  },
+  past = function(){
+
+  },
+  properties = function(){
+
+  },
+  permissions = function(){
+
+  },
+  versions = function(){
+
+  }
+
+}
