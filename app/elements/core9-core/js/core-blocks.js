698c304353f3d8dafcecc0ca6ca658ee8bfc93f4
Robert Cooke <robert.cooke@trimm.nl>
2015-07-26 16:26:25 +0200

    cc

--- app/elements/core9-core/js/core-blocks.js
+++ app/elements/core9-core/js/core-blocks.js
@@ -114,90 +114,32 @@ Core9.blocks.convertStringToHtml = function(string) {
   return d.firstChild;
 }
 
-Core9.blocks.insertBlock = function(progress, block, columnDiv, callback) {
-  callback(progress, block, columnDiv);
+
+Core9.blocks.insertBlock = function(block) {
+  var html = "<div class='core9-block' data-type='" + block + "'>" + block + "</div>";
+  var c = document.createComment("gm-editable-region");
+  columnDiv.appendChild(c);
+  columnDiv.appendChild(Core9.blocks.convertStringToHtml(html));
+  var c = document.createComment("/gm-editable-region");
+  columnDiv.appendChild(c);
 }
+
 
 Core9.blocks.init = function() {
-  function insert() {
-    var html = "<div class='core9-block' data-type='" + block[i].block + "'>" + block[i].block + "</div>";
-    var c = document.createComment("gm-editable-region");
-    columnDiv.appendChild(c);
-    columnDiv.appendChild(Core9.blocks.convertStringToHtml(html));
-    var c = document.createComment("/gm-editable-region");
-    columnDiv.appendChild(c);
-  }
+
 
   var data = {
     template: store.get('template') //"/dashboard/data/accounts/easydrain/themes/bower_components/core9-theme-ess/templates/pages/home/versions/blue/index.html"
   };
   var dataJson = Core9.blocks.__getDataJsonFromTemplate(data.template);
   Core9.blocks.__getJSON(dataJson, function(json) {
-    var blocks = json.blocks;
     var rows = document.getElementsByClassName('row');
-
-
+    var db = json;
+    Core9.blocks.loop('init', rows, db);
   });
-
-
 
 }
 
 
-Core9.blocks.init_org = function() {
-  var data = {
-    template: store.get('template') //"/dashboard/data/accounts/easydrain/themes/bower_components/core9-theme-ess/templates/pages/home/versions/blue/index.html"
-  };
-  var dataJson = Core9.blocks.__getDataJsonFromTemplate(data.template);
-  Core9.blocks.__getJSON(dataJson, function(json) {
-    var blocks = json.blocks;
-    var rows = document.getElementsByClassName('row');
-
-
-    var nrRows = rows.length;
-    for (var row = 0; row < nrRows; row++) {
-      var columns = rows[row].getElementsByClassName("column");
-      var nrColumns = columns.length;
-      for (var column = 0; column < nrColumns; column++) {
-        var columnDiv = columns[column];
-        try {
-          var progress = {
-              "nrrows": nrRows,
-              "row": row,
-              "nrcolumns": nrColumns,
-              "column": column
-            }
-            //console.log(blocks);
-          var block = blocks[row][column];
-          if (block) {
-            Core9.blocks.insertBlock(progress, block, columnDiv, function(progress, block, columnDiv) {
-              Core9.blocks.emptyElement(columnDiv);
-
-              for (var i = 0; i < block.length; i++) {
-
-                var html = "<div class='core9-block' data-type='" + block[i].block + "'>" + block[i].block + "</div>";
-                var c = document.createComment("gm-editable-region");
-                columnDiv.appendChild(c);
-                columnDiv.appendChild(Core9.blocks.convertStringToHtml(html));
-                var c = document.createComment("/gm-editable-region");
-                columnDiv.appendChild(c);
-
-              }
-
-
-            });
-          }
-        } catch (e) {}
-
-      }
-    }
-
-
-
-  });
-
-
-
-}
 
 Core9.blocks.init();
