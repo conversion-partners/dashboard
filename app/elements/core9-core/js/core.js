b2006534fbefcd26a2ebc948aad42ecbe15f71dd
Robert Cooke <robert.cooke@trimm.nl>
2015-08-02 13:16:01 +0200

    cc

--- app/elements/core9-core/js/core.js
+++ app/elements/core9-core/js/core.js
@@ -10,6 +10,15 @@ if (!String.prototype.format) {
     });
   };
 }
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+
 Core9 = {
 }
 
