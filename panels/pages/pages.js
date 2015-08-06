7c54eda73f16def2e916bbd57aa722527ddfcce6
Robert Cooke <robert.cooke@trimm.nl>
2015-08-06 11:28:53 +0200

    cc

--- panels/pages/pages.js
+++ panels/pages/pages.js
@@ -1,21 +1,21 @@
 TYPEOFPAGE = 'pages';
-var reduceFun = function (array) {
+var reduceFun = function(array) {
   return Core9.deDupeArray(array);
 }
 
 function getCountryOptions(theme) {
-  var mapFun = function (obj) {
+  var mapFun = function(obj) {
     return obj.country;
   }
   return Core9.data.templates.mapReduce(mapFun, reduceFun);
 }
 
 function getLanguageOptions(theme) {
-  var mapFun = function (obj) {
-    if(typeof theme == 'undefined') {
+  var mapFun = function(obj) {
+    if (typeof theme == 'undefined') {
       return obj.language;
     }
-    if(theme == obj.template) {
+    if (theme == obj.template) {
       return obj.language;
     }
   }
@@ -33,7 +33,7 @@ function getTemplateName(pageData) {
     "country": "" //$('#editor_holder2 > div > div.rows > div.col-md-10 > div:nth-child(1) > div.well.well-sm > div > div > div:nth-child(4) > div > div.form-group > select').val()
   }
   var result = Core9.data.templates.findObjects(query);
-  for(var i = 0; i < result.length; i++) {
+  for (var i = 0; i < result.length; i++) {
     var template = result[i];
     templateNames.push(template.page);
   }
@@ -55,17 +55,17 @@ function getTemplateVersion() {
   //
   return ['test'];
 }
-var activateEditor = function () {
+var activateEditor = function() {
   var pageData = getSelectBoxEntries()[0];
   var starting_value = pageData.versions;
   console.log('init Editor');
   console.log(pageData);
   try {
     Core9.editor2.destroy();
-  } catch(e) {}
+  } catch (e) {}
   try {
     Core9.editor.destroy();
-  } catch(e) {}
+  } catch (e) {}
   Core9.editor2 = new JSONEditor(document.getElementById('editor_holder'), {
     disable_edit_json: true,
     disable_collapse: true,
@@ -147,10 +147,10 @@ var activateEditor = function () {
       }
     }
   });
-  Core9.editor.on('change', function () {
+  Core9.editor.on('change', function() {
     var errors = Core9.editor.validate();
     var indicator = document.getElementById('valid_indicator');
-    if(errors.length) {
+    if (errors.length) {
       indicator.style.color = 'red';
       indicator.textContent = "not valid";
     } else {
@@ -172,59 +172,62 @@ var activateEditor = function () {
     Core9.data[TYPEOFPAGE].update(page);
     Core9.template.save();
   }
+
+
+  function getOptions(options) {
+    var optionStr = "";
+    var emptyOption = false;
+    for (var i = 0; i < options.length; i++) {
+      var option = options[i];
+      if (!isEmpty(option)) {
+        optionStr = optionStr.concat('<option value="' + option + '">' + option + '</option>');
+      } else {
+        emptyOption = true;
+      }
+    }
+    if (emptyOption) {
+      optionStr = '<option value=""></option>'.concat(optionStr);
+    }
+    return optionStr;
+  }
+
   // select boxes
   function watchVersion(version) {
-    $('[data-schemapath="root.0.language"]').find('select').prop('disabled', 'disabled');
-    $('[data-schemapath="root.0.country"]').find('select').prop('disabled', 'disabled');
-    $('[data-schemapath="root.0.template"]').find('select').prop('disabled', 'disabled');
-    $('[data-schemapath="root.0.version"]').find('select').prop('disabled', 'disabled');
-    Core9.editor.watch('root.' + version + '.theme', function () {
+    $('[data-schemapath="root.' + version + '.language"]').find('select').prop('disabled', 'disabled');
+    $('[data-schemapath="root.' + version + '.country"]').find('select').prop('disabled', 'disabled');
+    $('[data-schemapath="root.' + version + '.template"]').find('select').prop('disabled', 'disabled');
+    $('[data-schemapath="root.' + version + '.version"]').find('select').prop('disabled', 'disabled');
+
+    Core9.editor.watch('root.' + version + '.theme', function() {
       var language = Core9.editor.getEditor('root.' + version + '.language');
-      if(language) {
-        console.log('setting language');
-        var languageOptions = getLanguageOptions(Core9.editor.getEditor('root.' + version + '.theme').getValue());
-        var select = $('[data-schemapath="root.0.language"]').find('select');
-        $(select).on('change', function () {
-          console.log($(this).val());
+      if (language) {
+        var select = $('[data-schemapath="root.' + version + '.language"]').find('select');
+        $(select).on('change', function() {
           language.setValue($(this).val());
         });
-        $(select).empty();
-        var optionStr = "";
-        var emptyOption = false;
-        for(var i = 0; i < languageOptions.length; i++) {
-          var option = languageOptions[i];
-          if(!isEmpty(option)) {
-            optionStr = optionStr.concat('<option value="' + option + '">' + option + '</option>');
-          } else {
-            emptyOption = true;
-          }
-        }
-        if(emptyOption){
-          optionStr = '<option value=""></option>'.concat(optionStr);
-        }
-        $(select).append(optionStr);
-        $('[data-schemapath="root.0.language"]').find('select').prop('disabled', false);
+        $(select).empty().append(getOptions(getLanguageOptions(Core9.editor.getEditor('root.' + version + '.theme').getValue()))).prop('disabled', false);
       }
     });
-    Core9.editor.watch('root.' + version + '.language', function () {
+
+    Core9.editor.watch('root.' + version + '.language', function() {
       var language = Core9.editor.getEditor('root.' + version + '.language');
-      if(language) {
+      if (language) {
         console.log(language.getValue());
       }
       var country = Core9.editor.getEditor('root.' + version + '.country');
-      if(country) {
+      if (country) {
         country.setValue(" ");
       }
     });
-    Core9.editor.watch('root.' + version + '.country', function () {
+    Core9.editor.watch('root.' + version + '.country', function() {
       var template = Core9.editor.getEditor('root.' + version + '.template');
-      if(template) {
+      if (template) {
         template.setValue(" ");
       }
     });
-    Core9.editor.watch('root.' + version + '.template', function () {
+    Core9.editor.watch('root.' + version + '.template', function() {
       var templateVersion = Core9.editor.getEditor('root.' + version + '.version');
-      if(templateVersion) {
+      if (templateVersion) {
         templateVersion.setValue(" ");
       }
     });
@@ -234,10 +237,10 @@ var activateEditor = function () {
   watchVersion(2);
   watchVersion(3);
   // select boxes
-  document.getElementById('submit2').addEventListener('click', function () {
+  document.getElementById('submit2').addEventListener('click', function() {
     save();
   });
-  document.getElementById('restore2').addEventListener('click', function () {
+  document.getElementById('restore2').addEventListener('click', function() {
     Core9.editor.setValue(starting_value);
   });
 }
