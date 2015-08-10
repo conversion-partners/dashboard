b002dded8917d5bb470f8ee654f4af0501699a0f
Robert Cooke <robert.cooke@trimm.nl>
2015-08-10 22:23:22 +0200

    cc

--- app.js
+++ app.js
@@ -5,51 +5,50 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 var app = express();
 var mime = require('mime');
-
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({
   extended: true
 })); // for parsing application/x-www-form-urlencoded
 app.use(multer()); // for parsing multipart/form-data
-
-app.post('/api/io/:action', function(req, res) {
-  switch (req.params.action) {
-    case 'save':
-      fs.writeFile(".." + req.body.file, req.body.content, function(err) {
-        if (err) {
-          return console.log(err);
-        }
-        console.log("The file was saved!");
-      });
-      break;
-    default:
-      console.log(req);
+app.post('/api/io/:action', function (req, res) {
+  switch(req.params.action) {
+  case 'save':
+    fs.writeFile(".." + req.body.file, req.body.content, function (err) {
+      if(err) {
+        return console.log(err);
+      }
+      console.log("The file was saved!");
+    });
+    break;
+  default:
+    console.log(req);
   }
 });
-
-
-app.use('/dashboard/data/accounts/:account/themes/bower_components/:theme/templates/pages/:page/versions/:version/index.html', function(req, res) {
-
+app.use('/dashboard/data/accounts/:account/themes/bower_components/:theme/templates/pages/:page/versions/:version/index.html', function (req, res) {
   console.log(this);
-
-res.writeHead(200, {"Content-Type": "text/css"});
-
-fs.readFile(".." + req.originalUrl, 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
+  var file = '..' + req.originalUrl;
+  var contentType = mime.lookup(file);
+  // FIXME errorhandling
+  res.writeHead(200, {
+    "Content-Type": contentType
+  });
+  fs.readFile(file, 'utf8', function (err, data) {
+    if(err) {
+      // create new theme file
+      return console.log(err);
+    }
+    console.log(data);
+    res.write(data);
+    res.end();
+  });
 });
 
-});
 
 app.use('/dashboard/', express.static('.'));
-
-app.use('/*', function(req, res) {
+app.use('/*', function (req, res) {
   res.redirect('/dashboard/');
 });
-
-var server = app.listen(3000, function() {
+var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
