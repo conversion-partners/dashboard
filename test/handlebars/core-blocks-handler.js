712c5070a9b79760ef3c193b9a3995b6e84b9f46
Robert Cooke <robert.cooke@trimm.nl>
2015-08-14 11:14:44 +0200

    cc

--- test/handlebars/core-blocks-handler.js
+++ test/handlebars/core-blocks-handler.js
@@ -5,3 +5,15 @@ if (typeof Core9.blocks === 'undefined') {
   Core9.blocks = {}
 };
 Core9.blocks.handler = function() {}
+Core9.blocks.handler.getBlocks = function (){
+      return document.querySelectorAll(".core9-block");
+}
+Core9.blocks.handler.getData = function(){
+
+}
+Core9.blocks.handler.init = function(){
+    Core9.blocks.handler.getData();
+}
+
+
+Core9.blocks.handler.init();
