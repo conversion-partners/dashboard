6f45ef6a6854e121bf50aded394e95c58300a2ab
Robert Cooke <robert.cooke@trimm.nl>
2015-08-14 12:50:22 +0200

    cc

--- test/handlebars/core-blocks-handler.js
+++ test/handlebars/core-blocks-handler.js
@@ -1,3 +1,11 @@
+if(!String.prototype.format) {
+  String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+      return typeof args[number] != 'undefined' ? args[number] : match;
+    });
+  };
+}
 if(typeof Core9 === 'undefined') {
   Core9 = {}
 };
@@ -6,6 +14,19 @@ if(typeof Core9.blocks === 'undefined') {
   Core9.blocks = {}
 };
 Core9.blocks.handler = function () {}
+Core9.blocks.handler = {
+  paths: {
+    "blocks": "/dashboard/test/handlebars/blocks/"
+  }
+}
+Core9.blocks.handler.getData = function () {
+  Core9.blocks.handler.filRegistry().then(function (result) {
+    console.log(result);
+    return Core9.blocks.handler.getTemplateHtml(result);
+  }, function (err) {
+    console.log(err);
+  });
+}
 Core9.blocks.handler.j = function (url) {
   return new Promise(function (resolve, reject) {
     var xhr = new XMLHttpRequest;
@@ -15,16 +36,23 @@ Core9.blocks.handler.j = function (url) {
     xhr.send(null);
   });
 }
-Core9.blocks.handler.__registry = {}
+Core9.blocks.handler.__registry = []
 Core9.blocks.handler.filRegistry = function () {
-  var blocks = Core9.blocks.handler.getBlocks();
-  for(var i = 0; i < blocks.length; i++) {
-    var block = blocks[i];
-    Core9.blocks.handler.__registry.push({
-      "id": block.dataset.id,
-      "type": block.dataset.type
-    });
-  }
+  return new Promise(function (resolve, reject) {
+    var blocks = Core9.blocks.handler.getBlocks();
+    for(var i = 0; i < blocks.length; i++) {
+      var block = blocks[i];
+      Core9.blocks.handler.__registry.push({
+        "id": block.dataset.id,
+        "type": block.dataset.type
+      });
+    }
+    if(blocks.length == Core9.blocks.handler.__registry.length) {
+      resolve(Core9.blocks.handler.__registry);
+    } else {
+      reject(Error("It broke"));
+    }
+  });
 }
 Core9.blocks.handler.Handlebars = function () {
   if(typeof Handlebars == 'undefined') {
@@ -41,10 +69,10 @@ Core9.blocks.handler.getBlocks = function () {
 }
 Core9.blocks.handler.getFormSteps = function () {}
 Core9.blocks.handler.getJsonData = function () {}
-Core9.blocks.handler.getTemplateHtml = function () {}
-Core9.blocks.handler.getData = function () {
-
+Core9.blocks.handler.getTemplateHtml = function (listOfBlocks) {
+  return Core9.blocks.handler.j('/dashboard/test/handlebars/blocks/usermessage/tpl/index.html');
 }
+
 Core9.blocks.handler.init = function () {
   Core9.blocks.handler.getData();
 }
