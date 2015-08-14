c696c707d9981c647145015767feb547b5700383
Robert Cooke <robert.cooke@trimm.nl>
2015-08-14 11:52:16 +0200

    cc

--- test/handlebars/core-blocks-handler.js
+++ test/handlebars/core-blocks-handler.js
@@ -1,10 +1,22 @@
 if(typeof Core9 === 'undefined') {
   Core9 = {}
 };
+"use strict";
 if(typeof Core9.blocks === 'undefined') {
   Core9.blocks = {}
 };
 Core9.blocks.handler = function () {}
+Core9.blocks.handler.__registry = {}
+Core9.blocks.handler.filRegistry = function () {
+  var blocks = Core9.blocks.handler.getBlocks();
+  for(var i = 0; i < blocks.length; i++) {
+    var block = blocks[i];
+    Core9.blocks.handler.__registry.push({
+      "id": block.dataset.id,
+      "type": block.dataset.type
+    });
+  }
+}
 Core9.blocks.handler.Handlebars = function () {
   if(typeof Handlebars == 'undefined') {
     alert('no handlebars found');
@@ -22,7 +34,14 @@ Core9.blocks.handler.getFormSteps = function () {}
 Core9.blocks.handler.getJsonData = function () {}
 Core9.blocks.handler.getTemplateHtml = function () {}
 Core9.blocks.handler.getData = function () {
-  Core9.blocks.handler.triggerBlockDataReady();
+  var promiseList = [];
+  promiseList.push(Core9.blocks.handler.getTemplateHtml);
+  promiseList.push(Core9.blocks.handler.getJsonData);
+  promiseList.push(Core9.blocks.handler.getFormSteps);
+  promiseList.push(Core9.blocks.handler.filRegistry);
+  Promise.all(promiseList).then(function () {
+    console.log("all the promises are done");
+  });
 }
 Core9.blocks.handler.init = function () {
   Core9.blocks.handler.getData();
