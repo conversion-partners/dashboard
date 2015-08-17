6838610541b00e0f75168c739e8300b4b2d55ce6
Robert Cooke <robert.cooke@trimm.nl>
2015-08-17 21:30:59 +0200

    cc

--- app/elements/core9-core/js/core-blocks-forms.js
+++ app/elements/core9-core/js/core-blocks-forms.js
@@ -25,7 +25,7 @@ Core9.blocks.forms = {
     bower: "/dashboard/data/accounts/{0}/blocks/bower.json",
     template: "/dashboard/data/accounts/{0}/blocks/bower_components/{1}/tpl/hbs/templates.html",
     formSteps: "/dashboard/data/accounts/{0}/blocks/bower_components/{1}/forms/frontend/steps/steps.json",
-    formFilter: "/dashboard/data/accounts/{0}/blocks/bower_components/{1}/forms/frontend/steps/filters/filter.js",
+    formFilter: "/dashboard/data/accounts/{0}/blocks/bower_components/{1}/forms/frontend/steps/filters/",
     formDirectory: "/dashboard/data/accounts/{0}/blocks/bower_components/{1}/forms/frontend/steps/",
     defaultBlockData: "/dashboard/data/accounts/{0}/blocks/bower_components/{1}/forms/frontend/data/default.json",
     userDataById: "/dashboard/data/accounts/{0}/sites/pages/{1}/versions/{2}/data/{3}.json"
@@ -38,9 +38,12 @@ Core9.blocks.forms.__registry = {
 Core9.blocks.forms.getDataAndSchema = function (formFile) {}
 Core9.blocks.forms.getData = function (formData) {
   console.log('formdata : ');
-  if(typeof formData != 'undefined') {
-    console.log(formData.value);
-    var dataAndSchema = Core9.blocks.forms.getDataAndSchema(formData.value);
+  if(typeof formData == 'undefined') {
+    return;
+  }else{
+    var script = formData.value;
+    console.log(script);
+    var dataAndSchema = Core9.blocks.forms.getDataAndSchema(script);
   }
   var schema = {
     type: "object",
@@ -65,7 +68,7 @@ Core9.blocks.forms.getData = function (formData) {
   Core9.blocks.forms.filterForm(schema, data);
 }
 Core9.blocks.forms.filterForm = function (schema, data) {
-  var path = location.origin + Core9.blocks.forms.paths.formFilter.format(Core9.blocks.forms.config.account, Core9.blocks.forms.config.type); // "/dashboard/data/accounts/easydrain/blocks/bower_components/image/forms/frontend/steps/result/result.js";
+  var path = location.origin + Core9.blocks.forms.paths.formFilter.format(Core9.blocks.forms.config.account, Core9.blocks.forms.config.type) + 'form-data-organizer.js';
   var plugin = new jailed.Plugin(path);
   // called after the plugin is loaded
   var input = {
@@ -105,14 +108,42 @@ Core9.blocks.forms.loadForm = function (schema, data) {
   });
 
   function onSave() {
-    Core9.blocks.forms.saveForm(Core9.editor.getValue());
+    Core9.blocks.forms.saveForm(script, Core9.editor.getValue());
   }
   // Hook up the submit button to log to the console
   var saveButton = document.getElementById('submit');
   saveButton.removeEventListener('click', onSave, false);
   saveButton.addEventListener('click', onSave, false);
 }
-Core9.blocks.forms.saveForm = function (data) {
+Core9.blocks.forms.saveForm = function (script, data) {
+
+
+  var path = location.origin + Core9.blocks.forms.paths.formFilter.format(Core9.blocks.forms.config.account, Core9.blocks.forms.config.type) + 'save.js';
+  var plugin = new jailed.Plugin(path);
+  // called after the plugin is loaded
+  var input = {
+    schema: schema,
+    data: data
+  }
+  var start = function () {
+    // exported method is available at this point
+    plugin.remote.filter(input, reportResult);
+  }
+  var reportResult = function (result) {
+      console.log("Result is: ");
+      console.log(result);
+      var schema = result.schema;
+      var data = result.data;
+      if(result.action == 'submit'){
+        Core9.blocks.forms.saveData(schema, data);
+      }else{
+        // add to registry and processed next form
+      }
+
+    }
+    // execute start() upon the plugin is loaded
+  plugin.whenConnected(start);
+
   console.log(data);
 }
 Core9.blocks.forms.setSelectBox = function (formData) {
