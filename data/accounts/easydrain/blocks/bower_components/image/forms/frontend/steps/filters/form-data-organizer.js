7a044a8bf43f33f5642ed98a5170e52ee0427716
Robert Cooke <robert.cooke@trimm.nl>
2015-08-19 17:37:28 +0200

    cc

--- data/accounts/easydrain/blocks/bower_components/image/forms/frontend/steps/filters/form-data-organizer.js
+++ data/accounts/easydrain/blocks/bower_components/image/forms/frontend/steps/filters/form-data-organizer.js
@@ -1,19 +1,11 @@
-
 var stepData = {
-  "comments.json" : ["comments"],
-  "author.json" : ["author", "body"]
+  "comments.json": ["comments"],
+  "author.json": ["author", "body"]
 }
-
-
-
 var api = {
-    filter: function(input, cb) {
-        console.log('from form-data-organizer.js');
-        input.stepData = stepData;
-        console.log(input);
-        cb(input);
-    }
+  filter: function (input, cb) {
+    input.stepData = stepData;
+    cb(input);
+  }
 }
-
-// exports the api to the application environment
 application.setInterface(api);
