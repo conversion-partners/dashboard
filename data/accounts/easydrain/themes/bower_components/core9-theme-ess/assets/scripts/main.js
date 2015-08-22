000a65853da61a90f62f08b70decd923ac68846f
Robert Cooke <robert.cooke@trimm.nl>
2015-08-22 16:16:15 +0200

    cc

---
+++ data/accounts/easydrain/themes/bower_components/core9-theme-ess/assets/scripts/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+  'use strict';
+  $('.return-to-top').click(function () {
+    var body = $('html, body');
+    body.animate({
+      scrollTop: 0
+    }, '500', 'swing', function () {
+      console.log('Scrolled');
+    });
+  });
+  $(document).on('click', '.yamm .dropdown-menu', function (e) {
+    e.stopPropagation()
+  })
+  $(document).on('click', '.dropdown-toggle', function (e) {
+    console.log('menu clicked');
+    var menuLen = $('ul.nav > li.open').size();
+    if(menuLen == 0) {
+      $('.nav-bg').hide();
+      $('body').removeClass('noscroll');
+    } else {
+      $('html, body').animate({ scrollTop: 0 }, 0);
+      $('.nav-bg').show();
+      $('body').addClass('noscroll');
+    }
+    e.stopPropagation()
+  })
+})(jQuery);
