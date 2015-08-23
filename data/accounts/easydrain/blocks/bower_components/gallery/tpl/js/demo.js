92488c5b8369e3c92339bb726001ba4357835cfe
Robert Cooke <robert.cooke@trimm.nl>
2015-08-23 15:13:38 +0200

    cc

--- data/accounts/easydrain/blocks/bower_components/gallery/tpl/js/demo.js
+++ data/accounts/easydrain/blocks/bower_components/gallery/tpl/js/demo.js
@@ -2,14 +2,14 @@
 $(function() {
   $("#resizable").resizable({
     minWidth: 150,
-    maxWidth: 840,
+    maxWidth: 1640,
     grid: [10, 0],
     start: function() {
       $('.hint').fadeOut();
     },
     resize: function() {
       $('.live-classes').text(
-        $('[data-type="image"]').attr('class').replace('core9-block', '')
+        $('[data-type="gallery"]').attr('class').replace('core9-block', '')
       );
     }
   });
