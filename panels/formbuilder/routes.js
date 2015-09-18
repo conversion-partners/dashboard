d68a37e8c46664a1023a677921469e4effc38ea3
Robert Cooke <robert.cooke@trimm.nl>
2015-09-18 13:14:47 +0200

    cc

--- panels/formbuilder/routes.js
+++ panels/formbuilder/routes.js
@@ -1,7 +1,7 @@
 var formbuilderRoutes = {
   '/formbuilder$/': function(req) {
     Core9.panel.open('panel-formbuilder');
-    Core9.iframe.parent.sentMessageToIframe('console.log("hi");', Core9.panel.getIframeById('panel-formbuilder'));
+    Core9.iframe.parent.sentMessageToIframe('console.log("init formbuilder");', Core9.panel.getIframeById('panel-formbuilder'));
   }
 }
 Router.addRoutes(formbuilderRoutes);
