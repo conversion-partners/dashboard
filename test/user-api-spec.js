b128627ac6683b6683558e7a55d65abb19e0a591
Robert Cooke <robert.cooke@trimm.nl>
2015-10-27 15:04:59 +0100

    cc

--- test/user-api-spec.js
+++ test/user-api-spec.js
@@ -1,6 +1,11 @@
 require('co-mocha');
 var should = require('should');
 var data = require('../vendor/users/user-data.js');
+var fs = require('co-fs');
+
+before(function *(){
+  yield fs.writeFile('./test/data/users.json', '[]');
+});
 
 describe('user data', function(){
   it('should have +1 user count after saving', function* (){
