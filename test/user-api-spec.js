b9999d92a2d3f1a2562bd89e7a5d52653088bf97
Robert Cooke <robert.cooke@trimm.nl>
2015-10-27 14:11:14 +0100

    cc

--- test/user-api-spec.js
+++ test/user-api-spec.js
@@ -1,5 +1,13 @@
 
 
 describe('user data', function(){
-  
+  it('should have +1 user count after saving', function* (){
+    var users = yield data.users.get();
+
+    yield data.users.save({ name : 'John' });
+
+    var newUsers = yield data.users.get();
+
+    newUsers.lenght.should.equal(users.length + 1);
+  });
 })
