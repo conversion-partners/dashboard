136421568a6daf0d53047aa6158fa76582da6a90
Robert Cooke <robert.cooke@trimm.nl>
2015-12-11 23:22:08 +0100

    cc

--- app/elements/core9-menu/customsize/customsize.js
+++ app/elements/core9-menu/customsize/customsize.js
@@ -4,78 +4,55 @@ var arrMenu = [{
   icon: 'fa fa-reorder',
   items: []
 }];
-
-$(document).ready(
-  function() {
-
-
-
-
-    var body = document.body,
-      html = document.documentElement;
-
-    var height = Math.max(body.scrollHeight, body.offsetHeight,
-      html.clientHeight, html.scrollHeight, html.offsetHeight);
-
-    // HTML markup implementation, overlap mode
-    $('#menu').multilevelpushmenu({
-      menu: arrMenu,
-      menuWidth: 220, // '450px', '30em', '25%' will also work
-      menuHeight: height
-    });
-
-    var postClick = function() {
-      Core9.iframe.child.sentMessageToParent({
-        action: "menuClick",
-        href: this.href,
-        data: this.textContent
-      });
-    }
-    var addItemsToMenu = function(title, items) {
-      //console.log('adding to menu : ..');
-      $('a').unbind('click', postClick);
-      var $addTo = null;
-      try {
-        $addTo = $('#menu').multilevelpushmenu('findmenusbytitle',
-          title).first();
-      } catch (e) {
-        //console.log(e);
-      }
-
-      $('#menu').multilevelpushmenu('additems', items, $addTo, 0);
-
-      $('a').on('click', postClick);
-
-    }
-    $('a').on('click', postClick);
-    $('.fa-reorder').on('click', postClick);
-
-
-    var removeItemsFromMenu = function(title) {
-      var item = $('#menu').multilevelpushmenu('finditemsbyname',
-        title);
-      $('#menu').multilevelpushmenu('removeitems', item);
-    }
-
-
-    var callback = function(event) {
-      if (typeof event.data === 'undefined' || typeof event.data.action === 'undefined') return;
-      if (event.data.action == 'addItems') {
-        addItemsToMenu(event.data.findmenusbytitle,
-          event.data.addItems);
-
-          setTimeout(function(){
-
-            console.log('setting on click on heading');
-            $('h2').on('click',function(){
-              $('#menu').multilevelpushmenu('collapse', 0);
-            });
-
-          }, 2000);
-
-      }
-    }
-
-    Core9.iframe.child.listenToPostMessages(callback);
-
+$(document).ready(function () {
+  var body = document.body,
+    html = document.documentElement;
+  var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+  // HTML markup implementation, overlap mode
+  $('#menu').multilevelpushmenu({
+    menu: arrMenu,
+    menuWidth: 220, // '450px', '30em', '25%' will also work
+    menuHeight: height
   });
+  var postClick = function () {
+    Core9.iframe.child.sentMessageToParent({
+      action: "menuClick",
+      href: this.href,
+      data: this.textContent
+    });
+  }
+  var addItemsToMenu = function (title, items) {
+    //console.log('adding to menu : ..');
+    $('a').unbind('click', postClick);
+    var $addTo = null;
+    try {
+      $addTo = $('#menu').multilevelpushmenu('findmenusbytitle', title).first();
+    } catch(e) {
+      //console.log(e);
+    }
+    $('#menu').multilevelpushmenu('additems', items, $addTo, 0);
+    $('a').on('click', postClick);
+  }
+  $('a').on('click', postClick);
+  $('.fa-reorder').on('click', postClick);
+  var removeItemsFromMenu = function (title) {
+    var item = $('#menu').multilevelpushmenu('finditemsbyname', title);
+    $('#menu').multilevelpushmenu('removeitems', item);
+  }
+  var callback = function (event) {
+    if(typeof event.data === 'undefined' || typeof event.data.action === 'undefined') return;
+    if(event.data.action == 'addItems') {
+      addItemsToMenu(event.data.findmenusbytitle, event.data.addItems);
+      setTimeout(function () {
+        console.log('setting on click on heading');
+        $('h2').on('click', function () {
+          $('#menu').multilevelpushmenu('collapse', 0);
+        });
+        $('i.fa-cog').on('click', function () {
+          console.log(this);
+        });
+      }, 2000);
+    }
+  }
+  Core9.iframe.child.listenToPostMessages(callback);
+});
