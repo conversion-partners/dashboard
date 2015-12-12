ad62fe32721880b3029b7da0df19269fcc81b764
Robert Cooke <robert.cooke@trimm.nl>
2015-12-12 12:40:10 +0100

    cc

--- panels/blockpreview/routes.js
+++ panels/blockpreview/routes.js
@@ -2,6 +2,11 @@ var blockpreviewRoutes = {
   '/blockpreview/*': function (req) {
     Core9.panel.open('panel-blockpreview');
     console.log(req);
+
+    var iframe = Core9.panel.getIframeById('panel-blockpreview');
+    var cmd = 'initIframe('+JSON.stringify(location)+');';
+    Core9.iframe.parent.sentMessageToIframe(cmd, iframe);
+
   }
 }
 Router.addRoutes(blockpreviewRoutes);
