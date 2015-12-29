6d1b56b824a17b2d3b95f530a6d31846f744beda
Robert Cooke <robert.cooke@trimm.nl>
2015-12-29 01:04:10 +0100

    cc

--- git-servers.sh
+++ git-servers.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 
+cp config/servers/vps40041.public.cloudvps.com/files/config.json .
+cp config/servers/vps40041.public.cloudvps.com/files/gulpfile.js .
+cp config/servers/vps40041.public.cloudvps.com/files/panels/nide/data.json panels/nide/data.json
+
+
 #git add --all . -f
 git add .
 #git reset -- data/accounts/easydrain/sites/*
 git commit -m "cc"
 git push  origin HEAD:gh-pages
+
+
+cp config/servers/local/files/config.json .
+cp config/servers/local/files/gulpfile.js .
+cp config/servers/local/files/panels/nide/data.json panels/nide/data.json
