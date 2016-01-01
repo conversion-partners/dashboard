57d2106cdd4e91f8f7798cbfde402496dfd1c9b6
Robert Cooke <robert.cooke@trimm.nl>
2016-01-01 12:56:37 +0100

    cc

--- webdriver/scripts/test.js
+++ webdriver/scripts/test.js
@@ -1,5 +1,5 @@
 var webdriverio = require('webdriverio');
-var login = require('/var/www/dashboard/webdriver/scripts/modules/login/login.js');
+var login = require('./modules/login/login.js');
 var options = {
   desiredCapabilities: {
     browserName: 'chrome'
@@ -7,7 +7,11 @@ var options = {
 };
 var client = webdriverio.remote(options);
 client.addCommand("login", login.getFunction.bind(client));
-client.init();
-client.url('http://localhost:9090/dashboard/').login('test').saveScreenshot('./webdriver/images/screenshots/snapshot.png', function (err, screenshot, response) {}).title(function (err, res) {
-  console.log('Title was: ' + res.value);
-}).end();
+client.init()
+  .url('http://localhost:9090/dashboard/')
+  .login('test')
+  .saveScreenshot('./webdriver/images/screenshots/snapshot.png', function (err, screenshot, response) {})
+  .title(function (err, res) {
+    console.log('Title was: ' + res.value);
+  })
+  .end();
