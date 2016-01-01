98c8f4231823f68d854b3dc001d205eba03fc1fd
Robert Cooke <robert.cooke@trimm.nl>
2016-01-01 19:21:47 +0100

    cc

--- webdriver/scripts/modules/login/login.js
+++ webdriver/scripts/modules/login/login.js
@@ -1,11 +1,8 @@
 // tools.js
 // ========
 module.exports = {
-  getName: function () {
-    return "login";
-  },
   getFunction: function () {
-    console.log(arguments);
-    console.log(this);
+    //console.log(arguments);
+    //console.log(this);
   }
 };
--- webdriver/scripts/test.js
+++ webdriver/scripts/test.js
@@ -3,15 +3,42 @@ var login = require('./modules/login/login.js');
 var options = {
   desiredCapabilities: {
     browserName: 'chrome'
+      //browserName: 'phantomjs'
   }
 };
 var client = webdriverio.remote(options);
-client.addCommand("login", login.getFunction.bind(client));
+//client.addCommand("login", login.getFunction.bind(client));
 client.init()
   .url('http://localhost:9090/dashboard/')
-  .login('test')
-  .saveScreenshot('./webdriver/images/screenshots/snapshot.png', function (err, screenshot, response) {})
-  .title(function (err, res) {
-    console.log('Title was: ' + res.value);
-  })
-  .end();
+  //.windowHandleMaximize("current")
+  .then(function () {
+    setTimeout(function () {
+      client.element('#panel-login > iframe', function (err, res) {
+        client.frame(res.value)
+          .execute(function (a, b, c, d) {
+            document.querySelector('#user-email')
+              .value = "asdfsdf";
+            console.log(document.querySelector('#user-email')
+              .value);
+            document.querySelector('#user-pw')
+              .value = "asdfsdf";
+            console.log(document.querySelector('#user-pw')
+              .value);
+            //document.getElementsByClassName("simform")[0].submit();
+            document.getElementsByClassName("sumbit")[0].click();
+            // browser context - you may not access neither client nor console
+            return a + b + c + d;
+          }, 1, 2, 3, 4)
+          .then(function (ret) {
+            // node.js context - client and console are available
+            console.log(ret.value); // outputs: 10
+          })
+          .setValue('#user-email', 'test123')
+          .setValue('#user-pw', 'test123')
+          .click('.sumbit')
+          .getHTML('body')
+          .then(console.log)
+          .end();
+      });
+    }, 8000);
+  });
