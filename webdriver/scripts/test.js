87528ed16cc40edc2ff0912cec621373792b2197
Robert Cooke <robert.cooke@trimm.nl>
2016-01-01 12:48:46 +0100

    cc

--- webdriver/scripts/test.js
+++ webdriver/scripts/test.js
@@ -7,6 +7,7 @@ var options = {
 };
 var client = webdriverio.remote(options);
 client.addCommand("login", login.getFunction.bind(client));
-client.init().url('http://localhost:9090/dashboard/').login('test').saveScreenshot('./webdriver/images/screenshots/snapshot.png', function (err, screenshot, response) {}).title(function (err, res) {
+client.init();
+client.url('http://localhost:9090/dashboard/').login('test').saveScreenshot('./webdriver/images/screenshots/snapshot.png', function (err, screenshot, response) {}).title(function (err, res) {
   console.log('Title was: ' + res.value);
 }).end();
