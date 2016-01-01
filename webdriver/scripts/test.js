1f0fb6beaf650198847d77a3f5ac13af6f8510a9
Robert Cooke <robert.cooke@trimm.nl>
2016-01-01 22:59:59 +0100

    cc

--- webdriver/scripts/test.js
+++ webdriver/scripts/test.js
@@ -1,19 +1,34 @@
 var webdriverio = require('webdriverio');
 var login = require('./modules/login/login.js');
 var options = {
-  desiredCapabilities: {
-    //browserName: 'chrome'
-  browserName: 'phantomjs'
-  }
+  //desiredCapabilities: {
+  //browserName: 'chrome'
+  //browserName: 'phantomjs'
+  //}
+  capabilities: [{
+    browserName: 'phantomjs',
+    'phantomjs.cli.args': ['--ignore-ssl-errors=true', '--ssl-protocol=tlsv1', '--web-security=false', '--debug=false', '--disk-cache=true']
+  }]
 };
 var client = webdriverio.remote(options);
 //client.addCommand("login", login.getFunction.bind(client));
 client.init()
   .url('http://localhost:9090/dashboard/')
   //.windowHandleMaximize("current")
+  //.getHTML('body')
+  //.then(console.log)
   .then(function () {
     setTimeout(function () {
+
+      client.saveScreenshot('./webdriver/images/screenshots/snapshot.png', function (err, screenshot, response) {});
+      client.execute('return localStorage.getItem(arguments[0])', 'account')
+        .then((value) => console.log('storageKey = ' + JSON.stringify(value)));
+      client.element('#panel-login', function (err, res) {
+        //console.log(err);
+        //console.log(res);
+      });
       client.element('#panel-login > iframe', function (err, res) {
+        //console.log(err);
         client.frame(res.value)
           .execute(function (a, b, c, d) {
             document.querySelector('#user-email')
@@ -33,9 +48,7 @@ client.init()
             // node.js context - client and console are available
             console.log(ret.value); // outputs: 10
           })
-          .setValue('#user-email', 'test123')
-          .setValue('#user-pw', 'test123')
-          .click('.sumbit')
+          .saveScreenshot('./webdriver/images/screenshots/snapshot.png', function (err, screenshot, response) {})
           .getHTML('body')
           .then(console.log)
           .end();
