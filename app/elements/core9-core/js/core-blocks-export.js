e1147604a99e83a303b017ffc7474a4194ef8b9d
easydrain <jesse@accountshifter.com>
2016-01-10 15:13:57 +0100

    cc

--- app/elements/core9-core/js/core-blocks-export.js
+++ app/elements/core9-core/js/core-blocks-export.js
@@ -7,8 +7,28 @@ function removeIdsFromIframeContent(doc, idsToRemove) {
     } catch(err) {}
   }
 }
-Core9.exportPage = function () {
 
+function getMetaContentByName(doc, name, content) {
+  var content = (content == null) ? 'content' : content;
+  //.querySelector("meta[name='description']");
+  return doc.querySelector("meta[name='" + name + "']");
+  //.getAttribute(content);
+}
+
+function setMetaDataOnIframeContent(doc, metaData) {
+  doc.title = metaData.metadata.title;
+  //console.log(metaData);
+  var desc = getMetaContentByName(doc, "description");
+  //console.log(desc);
+  if(typeof desc != 'undefined') {
+    desc.parentNode.removeChild(desc);
+  }
+  var meta = document.createElement('meta');
+  meta.setAttribute('name', 'description');
+  meta.setAttribute('content', metaData.metadata.metadescription);
+  doc.getElementsByTagName('head')[0].appendChild(meta);
+}
+Core9.exportPage = function () {
     Core9.pageMetaData = JSON.parse(store.get('page-meta-data'));
     console.log(Core9.pageMetaData);
     var iframe = document.querySelector("#panel-iframe-site > iframe")
@@ -19,6 +39,7 @@ Core9.exportPage = function () {
       var doc = Core9.iframe.getContentAsDocument(iframe);
       var idsToRemove = ["js-boot", "js-boot-handler", "js-blocks", "js-child", "js-handlebars", "js-store", "css-demo", "css-bootstrap", "js-responsive-elements"];
       removeIdsFromIframeContent(doc, idsToRemove);
+      setMetaDataOnIframeContent(doc, Core9.pageMetaData);
       var content = Core9.iframe.documentToString(doc);
       var url = '/api/io/save';
       var file = store.get('cached-file')
@@ -42,7 +63,8 @@ Core9.exportPage = function () {
               .contentWindow;
             iframe.eval('Core9.blocks.handler.preAndPostSaving("postSave");');
             //document.getElementById("export-page")
-            document.getElementById("export-page").style.backgroundColor = "";
+            document.getElementById("export-page")
+              .style.backgroundColor = "";
           }
         }
       });
@@ -51,7 +73,8 @@ Core9.exportPage = function () {
   //export-page
 document.getElementById("export-page")
   .addEventListener("click", function () {
-    document.getElementById("export-page").style.backgroundColor = "green";
+    document.getElementById("export-page")
+      .style.backgroundColor = "green";
     var cachedFile = store.get('cached-file');
     if(typeof cachedFile != 'undefined') {
       Core9.exportPage();
