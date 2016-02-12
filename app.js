9e919e32fa4c05ef1e86ce2e5390b14ff1a34873
easydrain <jesse@accountshifter.com>
2016-02-12 21:44:28 +0100

    cc

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ passport.deserializeUser(function (id, cb) {
   });
 });
 // Configure view engine to render EJS templates.
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/panels/login/views');
 app.set('view engine', 'ejs');
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
