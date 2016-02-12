01d3e8e338b92816891b7245a3cce440014e9629
easydrain <jesse@accountshifter.com>
2016-02-12 15:52:15 +0100

    cc

--- app-org.js
+++ app-org.js
@@ -7,7 +7,6 @@ var app = express();
 var mime = require('mime');
 var mkdirp = require('mkdirp');
 var path = require('path');
-
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({
   extended: true
@@ -15,99 +14,6 @@ app.use(bodyParser.urlencoded({
 app.use(multer()); // for parsing multipart/form-data
 //app.use(express.json({limit: '50mb'}));
 //app.use(express.urlencoded({limit: '50mb'}));
-
-
-/** passport **/
-
-var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
-var db = require('./db');
-
-
-// Configure the local strategy for use by Passport.
-//
-// The local strategy require a `verify` function which receives the credentials
-// (`username` and `password`) submitted by the user.  The function must verify
-// that the password is correct and then invoke `cb` with a user object, which
-// will be set at `req.user` in route handlers after authentication.
-passport.use(new Strategy(
-  function(username, password, cb) {
-    db.users.findByUsername(username, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
-      return cb(null, user);
-    });
-  }));
-
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  The
-// typical implementation of this is as simple as supplying the user ID when
-// serializing, and querying the user record by ID from the database when
-// deserializing.
-passport.serializeUser(function(user, cb) {
-  cb(null, user.id);
-});
-
-passport.deserializeUser(function(id, cb) {
-  db.users.findById(id, function (err, user) {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
-
-// Configure view engine to render EJS templates.
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-//app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Define routes.
-app.get('/auth/',
-  function(req, res) {
-    res.render('home', { user: req.user });
-  });
-
-app.get('/auth/login',
-  function(req, res){
-    res.render('login');
-  });
-
-app.post('/auth/login',
-  passport.authenticate('local', { failureRedirect: '/auth/login' }),
-  function(req, res) {
-    res.redirect('/auth/');
-  });
-
-app.get('/auth/logout',
-  function(req, res){
-    req.logout();
-    res.redirect('/');
-  });
-
-app.get('/auth/profile',
-  require('connect-ensure-login').ensureLoggedIn(),
-  function(req, res){
-    res.render('profile', { user: req.user });
-  });
-
-/** passport **/
-
-
-
 var bodyParser = require('body-parser');
 app.use(bodyParser({
   limit: '200kb'
