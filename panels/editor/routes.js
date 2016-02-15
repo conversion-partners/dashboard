6a40d640487ad9956bd1139a13556610dd9b637a
easydrain <jesse@accountshifter.com>
2016-02-15 11:20:26 +0100

    cc

--- panels/editor/routes.js
+++ panels/editor/routes.js
@@ -1,12 +1,15 @@
 var gridRoutes = {
-  '/editor$/': function() {
+  '/editor$/': function () {
     Core9.panel.open('panel-iframe-site');
-    if (initPanelIframeSite) {
+    if(initPanelIframeSite) {
       var iframe = Core9.panel.getIframeById('panel-iframe-site');
       var cmd = 'window.gm = jQuery("#mycanvas").gridmanager().data("gridmanager");';
       Core9.iframe.parent.sentMessageToIframe(cmd, iframe);
       initPanelIframeSite = false;
     }
+  },
+  '/auth/logout': function () {
+    window.location.replace("/auth/logout");
   }
 }
 Router.addRoutes(gridRoutes);
--- panels/routes/routes.js
+++ panels/routes/routes.js
@@ -1,7 +1,7 @@
 var routes = {
-  '/home$/': function() {
+  '/home$/': function () {
     Core9.panel.open('panel-iframe-site');
-    if (initPanelIframeSite) {
+    if(initPanelIframeSite) {
       var iframe = Core9.panel.getIframeById('panel-iframe-site');
       var cmd = 'window.gm = jQuery("#mycanvas").gridmanager().data("gridmanager");';
       Core9.iframe.parent.sentMessageToIframe(cmd, iframe);
