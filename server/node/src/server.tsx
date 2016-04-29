148682d0f455da162662cc13b33518709c6280e7
easydrain <jesse@accountshifter.com>
2016-04-29 10:28:31 +0200

    cc

---
+++ server/node/src/server.tsx
@@ -0,0 +1,11 @@
+import * as root from "ts-npm-module";
+
+var foo = root.foo;
+var Bar = root.Bar;
+var bas = root.bas;
+
+console.log(bas);
+var para = document.createElement("p");
+var node = document.createTextNode(bas);
+document.getElementById("root").appendChild(node);
+console.log(foo);
