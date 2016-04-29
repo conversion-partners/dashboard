ba753b82eb786c985f402f658cfd854ad2dd62d6
easydrain <jesse@accountshifter.com>
2016-04-29 10:43:20 +0200

    cc

--- server/node/src/server.tsx
+++ server/node/src/server.tsx
@@ -1,13 +1,9 @@
-import * as root from "ts-npm-module";
+///<reference path='../../../server/node/typings/main/ambient/node/index.d.ts'/>
+import http = require("http");
 
-var foo = root.foo;
-var Bar = root.Bar;
-var bas = root.bas;
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
 
-var http = require("http");
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World" + bas);
-  response.end();
-}).listen(8888);
+console.log('Server running at http://127.0.0.1:1337/');
