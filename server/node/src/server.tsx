688d8e6cdb01b7d25efab4438ad878531700de91
easydrain <jesse@accountshifter.com>
2016-05-13 13:13:02 +0200

    cc

--- server/node/src/server.tsx
+++ server/node/src/server.tsx
@@ -1,17 +1,12 @@
 ///<reference path='../../../server/node/typings/main/ambient/node/index.d.ts'/>
 import http = require("http");
 
-
-
-
 import ns = require('ts-npm-module');
 var bas = ns.bas;
 
-
-
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World name : ' + bas);
+    res.end('Hello World name : ' + bas + " test");
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
