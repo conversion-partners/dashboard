2776c560741bd65812c0af218ed05695b70598ae
easydrain <jesse@accountshifter.com>
2016-05-14 08:54:12 +0200

    cc

---
+++ node_server/src/server.js
@@ -0,0 +1,13 @@
+"use strict";
+///<reference path='../typings/main.d.ts'/>
+var http = require("http");
+var bs = require('sos-server-config');
+var test = bs.foo2;
+var ns = require('ts-npm-module');
+var bas = ns.bas;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World name : ' + bas + " rtest");
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+//# sourceMappingURL=server.js.map
