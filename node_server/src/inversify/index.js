8c3930a32dd31dcf41b0e8ddb939a0ec7dfbaa33
easydrain <jesse@accountshifter.com>
2016-05-20 04:07:46 +0200

    cc

--- node_server/src/inversify/index.js
+++ node_server/src/inversify/index.js
@@ -1,8 +1,36 @@
-"use strict";
-const kernel_1 = require("./config/kernel");
-const types_1 = require("./constants/types");
-require("./config/wiring");
-let warrior = kernel_1.kernel.get(types_1.default.Warrior);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments)).next());
+    });
+};
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield ping();
+    });
+}
+function ping() {
+    return __awaiter(this, void 0, void 0, function* () {
+        for (var i = 0; i < 10; i++) {
+            yield delay(300);
+            console.log("ping");
+        }
+    });
+}
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+main();
+/*
+import { kernel } from "./config/kernel";
+import TYPES from "./constants/types";
+import "./config/wiring";
+
+let warrior = kernel.get<IWarrior>(TYPES.Warrior);
 let msg = warrior.fight();
 console.log(msg);
+
+*/
 //# sourceMappingURL=index.js.map
--- node_server/src/inversify/index.ts
+++ node_server/src/inversify/index.ts
@@ -1,3 +1,22 @@
+
+async function main() {
+ await ping();
+}
+
+async function ping() {
+ for (var i = 0; i < 10; i++) {
+  await delay(300);
+  console.log("ping");
+ }
+}
+
+function delay(ms: number) {
+ return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+main();
+
+/*
 import { kernel } from "./config/kernel";
 import TYPES from "./constants/types";
 import "./config/wiring";
@@ -5,3 +24,5 @@ import "./config/wiring";
 let warrior = kernel.get<IWarrior>(TYPES.Warrior);
 let msg = warrior.fight();
 console.log(msg);
+
+*/
